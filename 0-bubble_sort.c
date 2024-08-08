a41f0a1f22d7f3a7d0da8a138df3fa0a6c364d11
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-08 15:10:42 +0000

    header

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,15 @@
 #include "sort.h"
 /**
+ * bubble_sort - function bubble sort
+ * @array: array
+ * @size: size to array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t route, count;
 	int tmp;
 
-	for (route = 0; route < size -1; route++)
+	for (route = 0; route < size - 1; route++)
 	{
 		for (count = 0; count < size - 1 - route; count++)
 		{
