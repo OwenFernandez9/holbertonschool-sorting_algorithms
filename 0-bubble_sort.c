26e823bcf78909d6dbfb6b23c3de846d82233e12
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-08 15:08:05 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+/**
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t route, count;
+	int tmp;
+
+	for (route = 0; route < size -1; route++)
+	{
+		for (count = 0; count < size - 1 - route; count++)
+		{
+			if (array[count] > array[count + 1])
+			{
+				tmp = array[count];
+				array[count] = array[count + 1];
+				array[count + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
